ad7f6530d0bb9c3451efa40b45004f3330c47fc1
Jorge Fabila <46831682+J-Fabila@users.noreply.github.com>
2020-07-22 14:57:28 -0500

    Update atomic.hpp

--- atomic.hpp
+++ atomic.hpp
@@ -2576,7 +2576,7 @@ string string_pipe(string cmd,string defecto="AAA")
       pclose(stream);
    }
    data.pop_back();
-   if(data.length()>1)
+   if(data.length()>0)
    {
       return data;
    }
